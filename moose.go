feb9615898a224f1128ebf3244f668f29ca47692
Alexander F Rødseth <rodseth@gmail.com>
2016-04-04 15:32:20 +0200

    Consistent naming

--- moose.go
+++ moose.go
@@ -15,10 +15,10 @@ func NewMiddleware() *Middleware {
 }
 
 // The middleware handler
-func (l *Middleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+func (l *Middleware) ServeHTTP(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
 	// Log moose status
 	log.Printf("MOOSE: %v\n", l.moose)
 
 	// Call the next middleware handler
-	next(rw, r)
+	next(w, req)
 }
